d7782df69850ec7f2ef5240ccc5c6627f98a50f4
jasontrademarked <jasontrademarked@gmail.com>
2024-01-16 07:41:31 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+        music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    }
+})
